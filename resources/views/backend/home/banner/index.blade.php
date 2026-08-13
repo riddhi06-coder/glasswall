dc8675d0d5015fa3fd76a0b222291295b050d336
unknown <riddhi@matrixbricks.com>
2026-08-13 13:21:33 +0530

    Home Banner Details

--- resources/views/backend/home/banner/index.blade.php
+++ resources/views/backend/home/banner/index.blade.php
@@ -2,6 +2,29 @@
 <html lang="en">
 <head>
     @include('components.backend.head')
+    <style>
+        /* Banner heading is CKEditor rich-text; keep it readable in the list
+           instead of rendering giant <h1>/<h2> blocks. */
+        .banner-heading-cell {
+            font-size: 14px;
+            font-weight: 600;
+            line-height: 1.4;
+            max-width: 460px;
+            color: #2f2f3b;
+        }
+        .banner-heading-cell h1,
+        .banner-heading-cell h2,
+        .banner-heading-cell h3,
+        .banner-heading-cell h4,
+        .banner-heading-cell h5,
+        .banner-heading-cell h6,
+        .banner-heading-cell p {
+            font-size: inherit;
+            font-weight: inherit;
+            line-height: inherit;
+            margin: 0;
+        }
+    </style>
 </head>
 <body>
     @include('components.backend.header')
@@ -58,7 +81,8 @@
                                         @forelse($banners as $key => $banner)
                                             <tr>
                                                 <td>{{ $key + 1 }}</td>
-                                                <td>{!! $banner->banner_heading !!}</td>                                                <td>
+                                                <td><div class="banner-heading-cell">{!! $banner->banner_heading !!}</div></td>
+                                                <td>
                                                     @if($banner->media_type === 'image')
                                                         <img src="{{ asset('home/bannerimagevideo/'.$banner->banner_media) }}"
                                                              style="max-height: 120px;" class="img-fluid rounded" alt="banner">
@@ -69,14 +93,16 @@
                                                     @endif
                                                 </td>
                                                 <td>
-                                                    <a href="{{ route('banner-details.edit', $banner->id) }}" class="btn btn-sm btn-primary">Edit</a>
-                                                    <form action="{{ route('banner-details.destroy', $banner->id) }}"
-                                                          method="POST" style="display:inline-block"
-                                                          onsubmit="return confirm('Are you sure you want to delete this banner?')">
-                                                        @csrf
-                                                        @method('DELETE')
-                                                        <button class="btn btn-sm btn-danger">Delete</button>
-                                                    </form>
+                                                    <div class="d-flex gap-2">
+                                                        <a href="{{ route('banner-details.edit', $banner->id) }}" class="btn btn-sm btn-primary">Edit</a>
+                                                        <form action="{{ route('banner-details.destroy', $banner->id) }}"
+                                                              method="POST" class="m-0"
+                                                              onsubmit="return confirm('Are you sure you want to delete this banner?')">
+                                                            @csrf
+                                                            @method('DELETE')
+                                                            <button class="btn btn-sm btn-danger">Delete</button>
+                                                        </form>
+                                                    </div>
                                                 </td>
                                             </tr>
                                         @empty
